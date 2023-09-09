c4de457cdcf8a0bdd524e8933f251fa01077c390
Camila Freitas <camilafreitas.economia@gmail.com>
2023-09-09 15:34:14 -0300

    detalhes

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, Text, Date
-# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
 from decouple import config
 import pymysql
